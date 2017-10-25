bb5370cb9e38e32a853b2bfe6348608146c45f0b
rajat-jain9 <jainrajatcse@gmail.com>
2017-10-25 14:46:02 +0530

    test program

--- DataStructure/test.java
+++ DataStructure/test.java
@@ -1,10 +1,20 @@
 import java.util.*;
 public class test {
+    int b = 3;
+    void add(int a) {
+        a = a+3;//6
+        b = b+9; //12
+        System.out.println("add=" +a); //6
+    }
+    void printNum(int a) {
+        System.out.println("print=" +a); //3
+        System.out.println("b=" +b); //3+9=12
+    }
     public static void main(String arr[]) {
-        String s1 = "baap";
-        String s2 = "Baep";
-        if(s1.compareToIgnoreCase(s2) > 0) {
-            System.out.print("1");
-        }else System.out.print("2");
+        test obj = new test();
+        int a = 3;
+        obj.add(a);
+        System.out.println("main=" +a); //3
+        obj.printNum(a);
     }
 }
