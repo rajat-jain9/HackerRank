b57684f31ee290c93be4b3e3fb39040fce80ae83
rajat-jain9 <jainrajatcse@gmail.com>
2018-01-03 20:01:42 +0530

    added Armstrong Number Program

---
+++ ArmstrongNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ArmstrongNumber {
+  public static void main(String args[]) {
+  		Scanner sc = new Scanner(System.in);
+  		System.out.print("Enter a number: ");
+  		int num = sc.nextInt();
+  		int sum = 0;
+  		int r;
+  		int n = num;
+  		while(n != 0) {
+  			r = n % 10;
+  			sum = sum + r*r*r;
+  			n = n/10;
+  		}
+  		System.out.println("sum " +sum);
+  		System.out.println("num " +num);
+  		if(sum == num)
+  			System.out.println("Yes");
+  		else
+  				System.out.println("No");
+  }
+}
